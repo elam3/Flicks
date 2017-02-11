76569e5ea70e1e56a7bd5b1bb5a5a4e0c07730ab
Edison L <elam3@mail.ccsf.edu>
2017-02-11 08:21:56 -0800

    Added a scroll view     + add uiScrollView to DetailViewController     + re-arrange the 'z-axis' of the layers         + scrollView wraps all views     + move infoView off the screen     + add outlets for scrollView and infoView     + define scrollView contentSize using CGSize(width,height)

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -13,11 +13,17 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
         
         //print(movie)
         
